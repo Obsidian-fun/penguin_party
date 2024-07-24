937d6a700f5bd661ec7168404b6b00ae79c5e79e
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-07-24 11:36:27 +0530

    modified:   become_daemon.h

--- daemon/become_daemon.h
+++ daemon/become_daemon.h
@@ -7,10 +7,13 @@
 
 #define  BD_NO_CHDIR           01    /*Don't chdir, ie, don't change working directory*/
 #define  BD_NO_CLOSE_FILES     02    /*Don't close all open files*/
-#define  BD_NO_REOPEN_STD_FDS  04		 /**/
-#define  BD_NO_UMASKO          010   /**/
-#define  BD_MAX_CLOSE          8192  /**/
+#define  BD_NO_REOPEN_STD_FDS  04		 /*Don't reopen stdin, stdout, stderr to /dev/null */
+#define  BD_NO_UMASKO          010   /*Don't do a umask(O)*/
+#define  BD_MAX_CLOSE          8192  /*Maximum file descriptors to close if 
+																			sysconf(_SC_OPEN_MAX) is indeterminate*/
 
+int becomeDaemon(int flags);
 
+#endif
 
 
