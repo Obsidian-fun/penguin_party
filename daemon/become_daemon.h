1a25e435bed3949cc7532561b061b0801a407fd4
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-07-24 11:34:08 +0530

    modified:   become_daemon.h

--- daemon/become_daemon.h
+++ daemon/become_daemon.h
@@ -3,11 +3,13 @@
 #ifndef BECOME_DAEMON_H    /* to prevent double inclusion */
 #define BECOME_DAEMON_H
 
-#define  BD_NO_CHDIR           01
-#define  BD_NO_CLOSE_FILES     02
-#define  BD_NO_REOPEN_STD_FDS  04
-#define  BD_NO_UMASKO          010
-#define  BD_MAX_CLOSE          8192
+/* Bit-mask values for 'flags' argument of becomeDaemon()  */
+
+#define  BD_NO_CHDIR           01    /*Don't chdir, ie, don't change working directory*/
+#define  BD_NO_CLOSE_FILES     02    /*Don't close all open files*/
+#define  BD_NO_REOPEN_STD_FDS  04		 /**/
+#define  BD_NO_UMASKO          010   /**/
+#define  BD_MAX_CLOSE          8192  /**/
 
 
 
