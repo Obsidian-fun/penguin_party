ec9a14aa2ee3ee9afab3467a4809b7c10bdfa503
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-07-24 13:21:56 +0530

    modified:   become_daemon.c 	new file:   tlp_hdr.h

--- daemon/become_daemon.c
+++ daemon/become_daemon.c
@@ -3,4 +3,9 @@
 #include "sys/stat.h"
 #include "fcntl.h"
 #include "become_daemon.h"
+#include "tlpi_hdr.h"
 
+int becomeDaemon (int flags) {
+
+
+}
---
+++ daemon/tlp_hdr.h
@@ -0,0 +1,11 @@
+/* Book: Linux Programming Interface, Pg:51 */
+
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H
+
+#include "sys/types.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+
+
