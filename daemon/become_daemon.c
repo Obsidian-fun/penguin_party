2a7f897daf09f1bdadfeb4aea6da2def472bd197
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-07-24 20:43:56 +0530

    modified:   become_daemon.c

--- daemon/become_daemon.c
+++ daemon/become_daemon.c
@@ -5,7 +5,24 @@
 #include "become_daemon.h"
 #include "tlpi_hdr.h"
 
-int becomeDaemon (int flags) {
+int becomeDaemon (int flags) {   	/* returns 0 on success -1 on error*/
+	int maxfd, fd;
+
+	switch (fork()) { 							/* become background process */
+		case -1: return -1;
+		case 0 : break;								/*Child falls through*/
+		default: _exit(EXIT_SUCCESS);	/*While parent terminates*/
+	}
+
+	if (setsid == -1) {							/*Become leader of new session*/
+		return -1;
+	}
+
+	switch(fork()) {
+		case -1: return -1;
+		case 0: break;
+	  default: _exit(EXIT_SUCCESS);
+	}	
 
 
 }
