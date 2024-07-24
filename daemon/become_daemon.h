987b5538e80a12b4e72d3e4020f176d59511c24b
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-07-24 11:31:04 +0530

    modified:   become_daemon.h

---
+++ daemon/become_daemon.h
@@ -0,0 +1,14 @@
+/* Book: Linux Programming Interface, Pg:770 */
+
+#ifndef BECOME_DAEMON_H    /* to prevent double inclusion */
+#define BECOME_DAEMON_H
+
+#define  BD_NO_CHDIR           01
+#define  BD_NO_CLOSE_FILES     02
+#define  BD_NO_REOPEN_STD_FDS  04
+#define  BD_NO_UMASKO          010
+#define  BD_MAX_CLOSE          8192
+
+
+
+
